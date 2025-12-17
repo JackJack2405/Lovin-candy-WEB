66129bbc571214ab27aa27c7ad075b2a838f031b
klimmonmilk <tisirada@gmail.com>
2025-12-17 15:46:05 +0700

    checkpoint mymymy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Layout from "./components/Layout";
 import Home from "./views/Home";
 import MyProfile from "./views/MyProfile";
 import Customize from "./views/Customize";
+import MyOrder from "./views/MyOrder";
+import MyFavItems from "./views/MyFavItems";
+import MyAddress from "./views/MyAddress";
+import MyPayment from "./views/MyPayment";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +21,11 @@ const router = createBrowserRouter([
   children: [
     {path: "/", element: <Home />,},
     {path: "/customize", element: <Customize />,},
-    {path: "/myprofile", element: <MyProfile />,},
+    {path: "/profile", element: <MyProfile />,},
+    {path: "/profile/order", element: <MyOrder />,},
+    {path: "/profile/favitems", element: <MyFavItems />,},
+    {path: "/profile/address", element: <MyAddress />,},
+    {path: "/profile/payment", element: <MyPayment />,},
   ],
   },
 ]);
--- src/components/ProfileElementA.jsx
+++ src/components/ProfileElementA.jsx
@@ -1,28 +1,39 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./ProfileElement.css";
+import { TbNotes } from "react-icons/tb";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
+import { FiMapPin } from "react-icons/fi";
+import { PiMoneyBold } from "react-icons/pi";
 
 export default function ProfileElementA() {
+
+  const navLinkClass = ({ isActive }) => {
+      return isActive ? "itemsadj active" : "itemsadj";
+    };
+
   return (
+
     <div className="element-a">
-      <p>Order</p>
-      <ul>
-        <li>
-          <Link className="hover:"to="/myorder">My Order</Link>
+      <p className="heada">Order</p>
+      <ul className="ordera">
+        <li className="mt-2">
+          <NavLink className={navLinkClass} to="/profile/order" end><TbNotes/>My Order</NavLink>
         </li>
         <li>
-          <Link to="/favitems">Favourite items</Link>
+          <NavLink className={navLinkClass} to="/profile/favitems" end><FaRegHeart />Favourite items</NavLink>
         </li>
       </ul>
-      <p>Account</p>
-      <ul>
-        <li>
-          <Link to="/personalinformation">Personal Information</Link>
+      <p className="heada">Account</p>
+      <ul className="ordera1">
+        <li className="mt-2">
+          <NavLink className={navLinkClass} to="/profile" end><FaRegUser />Personal Information</NavLink>
         </li>
         <li>
-          <Link to="/address">Address</Link>
+          <NavLink className={navLinkClass} to="/profile/address" end><FiMapPin />Address</NavLink>
         </li>
         <li>
-          <Link to="/payment">Payment</Link>
+          <NavLink className={navLinkClass} to="/profile/payment" end><PiMoneyBold />Payment</NavLink>
         </li>
       </ul>
     </div>
