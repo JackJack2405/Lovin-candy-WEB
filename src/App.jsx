eb5eda6e1d19dd0e294be01f858bff3a10625e95
Macaron33-T <tarm3521@gmail.com>
2025-12-17 09:53:19 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import {createBrowserRouter, Router, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./views/Home";
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <Layout />,
-  errorElement: 
-    (
-    <div className="min-h-screen flex justify-center items-center bg-[#FAF3F3]">
-      <h1 className="text-4xl">404 Page Not Found</h1>
-    </div>
+    path: "/",
+    element: <Layout />,
+    errorElement: (
+      <div className="min-h-screen flex justify-center items-center bg-[#FAF3F3]">
+        <h1 className="text-4xl">404 Page Not Found</h1>
+      </div>
     ),
-  children: [
-    {path: "/", element: <Home />,},
-  ],
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
 
 function App() {
-
-
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
