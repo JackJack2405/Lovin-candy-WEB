de38d609f7f3d748a8fe0412fb4d2cda5faa612d
JackJack2405 <chutikankin7442@gmail.com>
2026-01-13 14:13:42 +0700

    added home_admin

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -1,8 +1,12 @@
+
 export default function AdminDashboard() {
-    return ( 
-        <div>
-            
-        </div>
-    )
-  
+ 
+
+  return (
+    <section className="w-full min-h-[calc(100vh-64px)] bg-[#FAF3F3] px-6 py-10">
+     
+
+      
+    </section>
+  );
 }
